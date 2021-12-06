14d0a628a0a34c0e0c722c9d838feeb9e5fbaee4
Stephen <stephen.s.espinosa@gmail.com>
2021-12-07 00:02:00 +0100

    added sass

--- module/mobile-dash.js
+++ module/mobile-dash.js
@@ -1,4 +1,4 @@
-"use strict";
+("use strict");
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -46,6 +46,8 @@ function displayKegInventory(kegData) {
   allKeg.forEach((element) => {
     console.log(element.name + ": " + element.amount);
   });
+
+  graphBeer(kegData);
 }
 
 function displayBartenders(bartenderData) {
@@ -67,3 +69,79 @@ function displayBartenders(bartenderData) {
 }
 
 // *****for graph*****
+
+function graphBeer(kegData) {
+  console.log(kegData.storage[0].amount);
+
+  if (window.matchMedia("(max-device-width: 414px)").matches) {
+    const ctx = document.getElementById("myChart");
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: [
+          kegData.storage[0].name,
+          kegData.storage[1].name,
+          kegData.storage[2].name,
+          kegData.storage[3].name,
+          kegData.storage[4].name,
+        ],
+
+        datasets: [
+          {
+            label: "Beer Order Sales",
+            data: [
+              kegData.storage[0].amount,
+              kegData.storage[1].amount,
+              kegData.storage[2].amount,
+              kegData.storage[3].amount,
+              kegData.storage[4].amount,
+            ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            labels: {
+              // This more specific font property overrides the global property
+              font: {
+                size: 14,
+              },
+            },
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+}
+
+// "El Hefe",
+// "Fairy Tale Ale",
+// "Mowintime",
+// "GitHop",
+// "Hollaback Lager",
+// "Hoppily Ever After",
+// "Row 26",
+// "Ruined Childhood",
+// "Sleighride",
+// "Steampunk",
