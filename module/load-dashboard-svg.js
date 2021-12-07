6b759e105ec29894135b4408c22b0913d6308d68
Filip <fili4252@stud.kea.dk>
2021-12-07 16:04:20 +0100

    loaded bar bgs on page

--- module/load-dashboard-svg.js
+++ module/load-dashboard-svg.js
@@ -1,3 +1,14 @@
 "use strict";
 
-export async function loadSvg() {}
+export async function loadSvg() {
+  const foregroundSvg = await fetch("assets/bar-foreground.svg");
+  const foregroundText = await foregroundSvg.text();
+  const midbackgroundSvg = await fetch("assets/bar-midbackground.svg");
+  const midbackgroundText = await midbackgroundSvg.text();
+  const bgSvg = await fetch("assets/bar-background.svg");
+  const bgText = await bgSvg.text();
+  //import to dom
+  document.querySelector(".bar-background").innerHTML = bgText;
+  document.querySelector(".bar-midbackground").innerHTML = midbackgroundText;
+  document.querySelector(".bar-foreground").innerHTML = foregroundText;
+}
