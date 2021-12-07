9d25873378d170ceff1ead5d6543198b7a148fdd
Stephen <stephen.s.espinosa@gmail.com>
2021-12-08 00:04:40 +0100

    final style for tap levels

--- module/displayTapLevels.js
+++ module/displayTapLevels.js
@@ -4,5 +4,29 @@ export function displayTapLevels(tapData) {
 
   allTap.forEach((element) => {
     console.log(element.beer + ": " + element.level);
+
+    let percentage = Math.round((parseInt(element.level) / 2500) * 100) + "%";
+
+    const clone = document
+      .querySelector("template#tap-levels")
+      .content.cloneNode(true);
+
+    clone.querySelector(".tap-name").textContent = element.beer;
+    clone.querySelector(".tap-percentage").textContent = percentage;
+
+    if (parseInt(percentage) <= 70 && parseInt(percentage) > 40) {
+      console.log(element.beer + " low");
+      clone.querySelector(".tap-percentage").style.color = "orange";
+      clone.querySelector(".tap-icon").src = "/assets/beer/low.svg";
+    } else if (parseInt(percentage) <= 40) {
+      console.log(element.beer + "  very low");
+      clone.querySelector(".tap-percentage").style.color = "red";
+      clone.querySelector(".tap-icon").src = "/assets/beer/very-low.svg";
+    } else {
+      clone.querySelector(".tap-percentage").style.color = "green";
+      clone.querySelector(".tap-icon").src = "/assets/beer/full.svg";
+    }
+
+    document.querySelector("#dash-tap .item-container").appendChild(clone);
   });
 }
