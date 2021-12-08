02f0e592e5be4b7facdeef8f81e971fa5362985c
Filip <fili4252@stud.kea.dk>
2021-12-08 11:18:18 +0100

    got bartender data and showed Klaus task in table

--- module/desktop-dash.js
+++ module/desktop-dash.js
@@ -14,10 +14,22 @@ function loadDynamicData() {
   const url = "https://hangover3.herokuapp.com/";
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showData(data));
+    .then((data) => getData(data));
   setTimeout(loadDynamicData, 1000);
 }
 
-function showData(data) {
-  console.log(data);
+function getData(data) {
+  const bartenderData = data.bartenders;
+  console.log(bartenderData);
+  showBartender(bartenderData[1]);
+}
+
+function showBartender(data) {
+  //change data in table
+  const nameCol = document.querySelector("td[data-bartender=name]");
+  nameCol.textContent = data.name;
+  const statusDetCol = document.querySelector("td[data-bartender=statusDetail]");
+  statusDetCol.textContent = data.statusDetail;
+  const tapCol = document.querySelector(`td[data-bartender=usingTap]`);
+  tapCol.textContent = data.usingTap;
 }
