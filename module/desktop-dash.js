c0d156f2f1399b0cc8d2918a59d6e2c99636e723
Filip <fili4252@stud.kea.dk>
2021-12-08 11:11:01 +0100

    fetch data every second

--- module/desktop-dash.js
+++ module/desktop-dash.js
@@ -1,5 +1,4 @@
 "use strict";
-
 import { loadSvg } from "./load-dashboard-svg";
 
 window.addEventListener("DOMContentLoaded", start);
@@ -7,4 +6,18 @@ window.addEventListener("DOMContentLoaded", start);
 function start() {
   console.log("start");
   loadSvg();
+  loadDynamicData();
+  setTimeout(loadDynamicData, 1000);
+}
+
+function loadDynamicData() {
+  const url = "https://hangover3.herokuapp.com/";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showData(data));
+  setTimeout(loadDynamicData, 1000);
+}
+
+function showData(data) {
+  console.log(data);
 }
