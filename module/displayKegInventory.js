08faca7c4082b09156061f2b9f445ade4b370e51
Stephen <stephen.s.espinosa@gmail.com>
2021-12-08 01:00:56 +0100

    finalizing keg-inventory style

--- module/displayKegInventory.js
+++ module/displayKegInventory.js
@@ -6,6 +6,23 @@ export function displayKegInventory(kegData) {
 
   allKeg.forEach((element) => {
     console.log(element.name + ": " + element.amount);
+
+    const clone = document
+      .querySelector("template#keg-inventory")
+      .content.cloneNode(true);
+
+    clone.querySelector(".keg-name").textContent = element.name;
+    clone.querySelector(".keg-left").textContent = element.amount;
+
+    if (parseInt(element.amount) <= 7 && parseInt(element.amount) > 4) {
+      clone.querySelector(".keg-left").style.color = "orange";
+    } else if (parseInt(element.amount) <= 4) {
+      clone.querySelector(".keg-left").style.color = "red";
+    } else {
+      clone.querySelector(".keg-left").style.color = "green";
+    }
+
+    document.querySelector("#dash-keg .item-container").appendChild(clone);
   });
 
   graphBeer(kegData);
