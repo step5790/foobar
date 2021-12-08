71478f84f42813a9caa4e979cdebca39deb7d6be
Stephen <stephen.s.espinosa@gmail.com>
2021-12-08 15:52:20 +0100

    modfiy displayQues.js and keg-invetory style

--- module/displayQues.js
+++ module/displayQues.js
@@ -1,13 +1,30 @@
 export function displayQues(queData) {
   console.log(queData.queue);
 
-  if (queData.queue.at(-1)) {
-    console.log("Ques:" + queData.queue.length);
-    console.log(queData.queue.at(-1).id);
+  let totalCustomer;
+  let partialTotalCustomer;
 
+  if (queData.queue.length != 0) {
+    if (queData.queue.at(-1)) {
+      console.log("Ques:" + queData.queue.length);
+      console.log(queData.queue.at(-1).id);
+
+      let partialTotalCustomer = queData.queue.at(-1).id;
+
+      document.querySelector(".item-1 .item-sub").innerHTML =
+        queData.queue.length;
+      document.querySelector(".item-2 .item-sub").innerHTML =
+        partialTotalCustomer;
+    }
+
+    totalCustomer = partialTotalCustomer;
+  }
+
+  if (queData.queue.length == 0) {
     document.querySelector(".item-1 .item-sub").innerHTML =
       queData.queue.length;
-    document.querySelector(".item-2 .item-sub").innerHTML =
-      queData.queue.at(-1).id;
+    // document.querySelector(".item-2 .item-sub").innerHTML = totalCustomer;
   }
+
+  console.log("total: " + totalCustomer);
 }
