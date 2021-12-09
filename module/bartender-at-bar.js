efc364a4b2e5bfa96f6ff8497d9727494a99407e
Filip <fili4252@stud.kea.dk>
2021-12-09 16:48:45 +0100

    bt move to next tap

--- module/bartender-at-bar.js
+++ module/bartender-at-bar.js
@@ -15,8 +15,10 @@ export function getBartenderAndTap(bartender, firstTap) {
     console.log("bartender needs to change a tap");
     //move bartender to new tap
     const bt = getBartenderAtBar(bartender);
+    console.log(bt);
     //change display to bartender with beer(s)
     //remove bartender from old tap
+    animateToTap(tap.element, tap.position, bt.element, bt.position, bartender);
   }
 }
 
@@ -28,5 +30,11 @@ function getBartenderAtCounter(bartender) {
 
 function getBartenderAtBar(bartender) {
   const bartenderInDom = document.querySelector(`g#${bartender.name}_pouring`).closest("div.bt-pouring");
-  console.log(bartenderInDom);
+  if (bartenderInDom === null) {
+    console.log("bartender hasn't been created at counter yet");
+    getBartenderAtCounter(bartender);
+  } else {
+    const bartenderPosition = bartenderInDom.getBoundingClientRect();
+    return { element: bartenderInDom, position: bartenderPosition };
+  }
 }
