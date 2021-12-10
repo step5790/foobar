fc568f19516a2d2f4da5eae6adee542cd3c39170
Filip <fili4252@stud.kea.dk>
2021-12-10 13:16:05 +0100

    removed console-logs

--- module/get-bartender.js
+++ module/get-bartender.js
@@ -1,14 +1,12 @@
 "use strict";
 
 export function getBartenderAtBar(bartender) {
-  console.log("get bartender at BAR");
   const bartenderInDom = document.querySelector(`g#${bartender.name}_pouring`).closest("div.bt-pouring");
   const bartenderPosition = bartenderInDom.getBoundingClientRect();
   return { element: bartenderInDom, position: bartenderPosition };
 }
 
 export function getBartenderSpotAtCounter(bartender) {
-  console.log("get bartender spot at COUNTER");
   const counterContainerInDom = document.querySelector(`div#${bartender.name}`);
   const counterPosition = counterContainerInDom.getBoundingClientRect();
   return { element: counterContainerInDom, position: counterPosition };
