c13a1f8dd771dffccb2133a85e200b0fc277f993
Filip <fili4252@stud.kea.dk>
2021-12-10 14:29:05 +0100

    made function to handle tap after move-animation and put it in module, unhid tap when used

--- module/get-tap.js
+++ module/get-tap.js
@@ -5,3 +5,14 @@ export function getTap(tap) {
   const tapG = document.querySelector(`g#tap_${tapNumberAsString}`);
   return tapG;
 }
+
+export function handleTap(bartender) {
+  //get tap element
+  const tap = getTap(bartender.usingTap);
+  console.log(tap);
+  //unhide beer glass g
+  tap.querySelector(".beer_front").style.visibility = "visible";
+
+  //start pouring animation
+  //hide tap if newStatus === "releaseTap"
+}
--- module/move-bartender.js
+++ module/move-bartender.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { importBartenderSvg } from "./import-bartender-svg";
-import { getTap } from "./get-tap";
+import { getTap, handleTap } from "./get-tap";
 
 export function animateToDestination(destinationElement, destinationPosition, btElement, btPosition, bartender) {
   //calculate the movement
@@ -27,13 +27,7 @@ export function animateToDestination(destinationElement, destinationPosition, bt
     if (bartender.statusDetail === "pourBeer") {
       btElement.firstElementChild.innerHTML = "";
       importBartenderSvg(bartender, "pouring", btElement);
-      //get tap element
-      const tap = getTap(bartender.usingTap);
-      console.log(tap);
-
-      //unhide beer glass g
-      //start pouring animation
-      //hide tap if newStatus === "releaseTap"
+      handleTap(bartender);
     }
     //remove bt at bar and import at the counter
     else if (bartender.statusDetail === "receivePayment" || bartender.statusDetail === "reserveTap") {
