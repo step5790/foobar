5e9c196caf76baf1096caf554a2fe638adbf69b8
Filip <fili4252@stud.kea.dk>
2021-12-11 22:44:34 +0100

    made four customers and imported scarlet.svg in each div

--- module/load-dashboard-svg.js
+++ module/load-dashboard-svg.js
@@ -9,19 +9,19 @@ export async function loadSvg() {
   const midbackgroundText = await midbackgroundSvg.text();
   const bgSvg = await fetch("assets/bar-background.svg");
   const bgText = await bgSvg.text();
-  const klausLeaningSvg = await fetch("assets/bartenders/klaus-leaning.svg");
-  const klausLeaningText = await klausLeaningSvg.text();
-  const klausFrontSvg = await fetch("assets/bartenders/klaus-front.svg");
-  const klausFrontText = await klausFrontSvg.text();
+  //customers
+  const scarletSvg = await fetch("assets/customers/scarlet.svg");
+  const scarletSvgText = await scarletSvg.text();
   //import to dom
   document.querySelector(".bar-background").innerHTML = bgText;
   document.querySelector(".bar-midbackground").innerHTML = midbackgroundText;
   document.querySelector(".bar-foreground").innerHTML = foregroundText;
   // TODO:
   //add each svg to bt object in bt array
-  // //bartenders
-  // document.querySelector("#Klaus .front").innerHTML = klausFrontText;
-  // document.querySelector("#Klaus .leaning").innerHTML = klausLeaningText;
+  // import customers in DOM
+  document.querySelectorAll(".customer").forEach((element) => {
+    element.innerHTML = scarletSvgText;
+  });
 }
 
 export async function loadBartenderSvg(name, display) {
