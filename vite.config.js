7f6daf1896cf076a4a2e7bef860d685569e8a12f
Stephen <stephen.s.espinosa@gmail.com>
2021-12-12 21:41:45 +0100

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+module.exports = {
+  base: "./", //set base here or in the build command
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sub: resolve(__dirname, "productlist.html"),
+      },
+    },
+  },
+};
