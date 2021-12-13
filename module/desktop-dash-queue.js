7223333f1f112d119352569c4523b97bc0f4e165
Filip <fili4252@stud.kea.dk>
2021-12-13 12:40:00 +0100

    add and remove queue elements based on difference

--- module/desktop-dash-queue.js
+++ module/desktop-dash-queue.js
@@ -11,18 +11,22 @@ export function createQueue(queueData, svgs) {
     //calculate difference between how many was and grew with
     const difference = calculateDifference(queue, oldQueue);
     console.log("should add", difference, "svgs in que");
-    //send the difference as params to addQueue
+
     //check if queue is longer than 8
     //if yes: add queue groups
     //if no:
-    addQueue(svgs);
+    //send the difference as params to addQueue
+    addQueue(svgs, difference);
   } else if (queue < oldQueue) {
     console.log("Queue became SHORTER and is now", queue, "long");
     //check if length is not 0
     if (oldQueue > 0) {
       //calculate difference between how many was and grew with
+      const difference = calculateDifference(queue, oldQueue);
+      console.log("should remove", difference, "svgs from que");
       //send the difference as params to removeQueue
-      // removeQueue();
+
+      removeQueue(difference);
     }
   } else {
     console.log("Queue length did not change and is still", queue);
@@ -34,19 +38,24 @@ function calculateDifference(a, b) {
   return Math.abs(a - b);
 }
 
-function addQueue(svgs) {
+function addQueue(svgs, max) {
   // grap parent
   const parent = document.querySelector(".queue");
-  // create a new div element
-  const newQueueElement = document.createElement("div");
-  newQueueElement.innerHTML = svgs[1];
-  // add the newly created element and its content into the DOM
-  parent.appendChild(newQueueElement);
-  console.log("created newQueueElement");
+  //loop max times
+  for (let i = 0; i < max; i++) {
+    // create a new div element
+    const newQueueElement = document.createElement("div");
+    newQueueElement.innerHTML = svgs[1];
+    // add the newly created element and its content into the DOM
+    parent.appendChild(newQueueElement);
+  }
+  console.log("created newQueueElement", max, "times");
 }
 
-function removeQueue() {
+function removeQueue(max) {
   const parent = document.querySelector(".queue");
-  parent.lastElementChild.remove();
-  console.log("removed lastElementChild");
+  for (let i = 0; i < max; i++) {
+    parent.lastElementChild.remove();
+  }
+  console.log("removed lastElementChild", max, "times");
 }
