03864b6706a2293650a438134ce9b9c703223b34
Filip <fili4252@stud.kea.dk>
2021-12-13 12:02:57 +0100

    made functions to create and remove an element with svg inside the queue based on changes

--- module/desktop-dash-queue.js
+++ module/desktop-dash-queue.js
@@ -2,14 +2,40 @@
 
 let oldQueue = 0;
 
-export function createQueue(queueData, queueSvgs) {
+export function createQueue(queueData, svgs) {
   const queue = queueData.length;
+
   if (queue > oldQueue) {
     console.log("Queue became LONGER and is now", queue, "long");
+    //TODO: check if queue is longer than 8
+    //if yes: add queue groups
+    //if no:
+    addQueue(svgs);
   } else if (queue < oldQueue) {
     console.log("Queue became SHORTER and is now", queue, "long");
+    //check if length is not 0
+    if (queueData.length !== 0) {
+      removeQueue(queueData.length);
+    }
   } else {
     console.log("Queue length did not change and is still", queue);
   }
   oldQueue = queue;
+}
+
+//TODO: try load svgs again, the old way
+
+function addQueue(svgs) {
+  // grap parent
+  const parent = document.querySelector(".queue");
+  // create a new div element
+  const newQueueElement = document.createElement("div");
+  newQueueElement.innerHTML = svgs[1];
+  // add the newly created element and its content into the DOM
+  parent.appendChild(newQueueElement);
+}
+
+function removeQueue() {
+  const parent = document.querySelector(".queue");
+  parent.lastElementChild.remove();
 }
