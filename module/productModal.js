5fe85d3ab9392ea2fdaac6d7b2e77d48705aae25
peterstastny <peter.stastny7@gmail.com>
2021-12-13 21:04:07 +0100

    added feedback when clicking plus and minus

--- module/productModal.js
+++ module/productModal.js
@@ -1,20 +1,25 @@
-export function registerModal() {
-  const beers = document.querySelectorAll(".beer");
-
-  for (const beer of beers) {
-    beer.addEventListener("click", toggleModal);
-  }
-}
-
-export function toggleModal() {
+export function toggleModal(beersData) {
+  //console.log(this.dataset.name);
   document.querySelector("#modal").classList.remove("hidden");
   document.querySelector("#pageMask").classList.remove("hidden");
   document.querySelector("#productlist").classList.add("noScroll");
   document.querySelector(".exitModal").addEventListener("click", closeModal);
+
+  changeBeerData(beersData);
+  //to do
+
+  //change content with one beer
+  //loop through the beers and find the matching - index of array
+  //change he content with the data from the index
 
   function closeModal() {
     document.querySelector("#modal").classList.add("hidden");
     document.querySelector("#pageMask").classList.add("hidden");
     document.querySelector("#productlist").classList.remove("noScroll");
   }
+}
+
+function changeBeerData(beersData) {
+  console.log(beersData);
+  document.querySelector(".productModalHeading").textContent = beersData[0].name;
 }
