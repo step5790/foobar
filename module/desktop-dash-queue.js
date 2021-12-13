6837cc55b0138c4b2ac2e37c94aea89e62defd14
Filip <fili4252@stud.kea.dk>
2021-12-13 13:28:22 +0100

    check if svg in use (in array) and add the svg index after creating it, also remove first item from array when removed

--- module/desktop-dash-queue.js
+++ module/desktop-dash-queue.js
@@ -49,23 +49,36 @@ function addQueue(svgs, max) {
 function createQueueSvg(svgs) {
   //get random customer from array based on arr length
   const randomIndex = getRandomInt(svgs.length);
-  // grap parent
-  const parent = document.querySelector(".queue");
-  // create a new div element
-  const newQueueElement = document.createElement("div");
+  //check that index is not in use
+  const isInUse = checkIfSvgInUse(randomIndex);
+  if (isInUse === false) {
+    // grab parent
+    const parent = document.querySelector(".queue");
+    // create a new div element
+    const newQueueElement = document.createElement("div");
 
-  //create inside element
-  newQueueElement.innerHTML = svgs[randomIndex];
-  //push index in the array of svgsInUse
-  svgsInUse.push(randomIndex);
-  // add the newly created element and its content into the DOM
-  parent.appendChild(newQueueElement);
+    //create inside element
+    newQueueElement.innerHTML = svgs[randomIndex];
+    //push index in the array of svgsInUse
+
+    // add the newly created element and its content into the DOM
+    parent.appendChild(newQueueElement);
+  } else {
+    console.log(randomIndex, "svg is already in use");
+    createQueueSvg(svgs);
+  }
+}
+
+function checkIfSvgInUse(number) {
+  return svgsInUse.some((index) => index === number);
 }
 
 function removeQueue(max) {
   const parent = document.querySelector(".queue");
   for (let i = 0; i < max; i++) {
     parent.firstElementChild.remove();
+    //remove first element in array
+    svgsInUse.shift();
   }
   console.log("removed lastElementChild", max, "times");
 }
