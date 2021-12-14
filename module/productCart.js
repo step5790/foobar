0d970f6c1af169ea35466412287f7e8dbedfdd96
Filip Soudakov <fili4252@stud.kea.dk>
2021-12-14 18:19:29 +0100

    moved cart js to productCart.js

--- module/productCart.js
+++ module/productCart.js
@@ -15,3 +15,47 @@ function toggleCart() {
     document.querySelector("#productlist").classList.remove("noScroll");
   }
 }
+
+("use strict");
+
+const order = [];
+
+export function addToCart(singleOrder) {
+  console.log("add", singleOrder, "to cart");
+  //check if beer is already in array
+  if (order.some((el) => el.beer.name === singleOrder.beer.name)) {
+    console.log(singleOrder.beer.name, "is already added");
+    //find beer in array
+    const indexOfBeer = order.findIndex((obj) => obj.beer.name === singleOrder.beer.name);
+    order[indexOfBeer].quantity = order[indexOfBeer].quantity + singleOrder.quantity;
+    console.log("new quantity:", order[indexOfBeer].quantity);
+  } else {
+    order.push(singleOrder);
+  }
+  console.log("updated order:", order);
+  //display cart items
+  order.forEach((obj) => {
+    displayCartItem(obj);
+  });
+}
+
+/* <div class="cartProduct">
+        <img class="cartProductImage" src="assets/beer/hollaback.png" alt="Hollaback Lager" />
+        <div class="cartNameAmount">
+          <p class="cartName">Hollaback Lager</p>
+          <div class="adjustAmount">
+            <img class="modalMinus" src="assets/icons/minus.svg" />
+            <p class="beerQuantity">1</p>
+            <img class="modalPlus" src="assets/icons/plus.svg" />
+          </div>
+        </div>
+        <p class="cartPrice">40 DKK</p>
+        <button class="modalRemoveCart"></button>
+      </div> */
+
+function displayCartItem(obj) {
+  const template = document.querySelector("#cartTemp").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".cartProductImage").src = obj.beer.label;
+  // const parent = document.querySelector("#");
+}
