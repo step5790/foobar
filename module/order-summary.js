9f106c8b5c68a0b2a24153c2b89a0e2c727c75e9
Filip Soudakov <fili4252@stud.kea.dk>
2021-12-15 16:05:53 +0100

    made order summary dynamic

---
+++ module/order-summary.js
@@ -0,0 +1,38 @@
+"use strict";
+
+import { calculateBasePrice, calculateSubTotal, calculateTotal } from "./beer-price";
+
+export function populateOrderSummary() {
+  console.log("ready");
+  //check if local storage has order
+  if (!localStorage.getItem("order")) {
+    console.log("cart is empty");
+  } else {
+    const order = JSON.parse(localStorage.getItem("order"));
+    console.log(order);
+    displaySummary(order);
+    displayTotal(order);
+  }
+}
+
+function displaySummary(order) {
+  //clear parent
+  const parent = document.querySelector(".cart-details table");
+  parent.innerHTML = "";
+  //display each
+  order.forEach((obj) => displaySummaryItem(parent, obj));
+}
+
+function displaySummaryItem(parent, obj) {
+  const template = document.querySelector("#orderItemTemp").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("img").src = `assets/beer/${obj.beer.label}`;
+  copy.querySelector(".beer-name").textContent = obj.beer.name;
+  copy.querySelector(".quantity").textContent = obj.quantity;
+  copy.querySelector(".sub-total").textContent = `${calculateSubTotal(calculateBasePrice(obj.beer.alc), obj)} DKK`;
+  parent.appendChild(copy);
+}
+
+function displayTotal(order) {
+  document.querySelector(".beer-total").textContent = `${calculateTotal(order)} DKK`;
+}
