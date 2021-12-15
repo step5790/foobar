e16dfde3a55bb3b4f4e4e8f89ef503a63d305516
Stephen <stephen.s.espinosa@gmail.com>
2021-12-15 22:09:14 +0100

    added thank-you

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ module.exports = {
         sub1: resolve(__dirname, "form-login.html"),
         sub2: resolve(__dirname, "dashboard-desktop.html"),
         sub3: resolve(__dirname, "dashboard.html"),
+        sub4: resolve(__dirname, "thank-you.html"),
       },
     },
   },
