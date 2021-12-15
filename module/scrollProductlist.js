06b02a897180ff402f69792816730ed77fd945ea
Filip Soudakov <fili4252@stud.kea.dk>
2021-12-15 14:58:59 +0100

    made arrows change opacity dynamically based on scroll position

--- module/scrollProductlist.js
+++ module/scrollProductlist.js
@@ -6,14 +6,22 @@ export function scrollProductlist() {
 export function scrollInitial() {
   const firstBeer = document.querySelector(".beersContainer").firstElementChild;
   firstBeer.scrollTo(0, 0);
+  //hide left scroll arrow
+  document.querySelector(".scrollArrow1").style.opacity = 0.3;
 }
 
 function scrollRight() {
   const lastBeer = document.querySelector(".beersContainer").lastElementChild;
   lastBeer.scrollIntoView({ behavior: "smooth", block: "center" });
+  //hide right arrow
+  document.querySelector(".scrollArrow1").style.opacity = 1;
+  document.querySelector(".scrollArrow2").style.opacity = 0.3;
 }
 
 function scrollLeft() {
   const firstBeer = document.querySelector(".beersContainer").firstElementChild;
   firstBeer.scrollIntoView({ behavior: "smooth", block: "center" });
+  //hide left arrow
+  document.querySelector(".scrollArrow1").style.opacity = 0.3;
+  document.querySelector(".scrollArrow2").style.opacity = 1;
 }
